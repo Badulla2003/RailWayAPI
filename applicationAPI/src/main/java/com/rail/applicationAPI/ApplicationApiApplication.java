7fc676e45f6243d6f0237cdf4e80654f1ce039f2
Badulla <siaramayodya@gmail.com>
2025-05-14 12:36:45 +0530

    commit

---
+++ applicationAPI/src/main/java/com/rail/applicationAPI/ApplicationApiApplication.java
@@ -0,0 +1,21 @@
+package com.rail.applicationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApplicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationApiApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String hi() {
+		return "hello world";
+	}
+
+}
---
+++ applicationAPI/src/test/java/com/rail/applicationAPI/ApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rail.applicationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
